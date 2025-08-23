3cc14956ecd3dbc0f20ddc15557327ab7743f497
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2025-08-23 10:51:21 -0600

    feat: button logout disappears when the user is in the login page

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,12 +1,16 @@
 import { LogOut } from 'lucide-react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from '../../auth/store/authSlice';
 import { useNavigate } from 'react-router';
+import type { RootState } from '../../app';
 
 export function Navbar() {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {access_token} = useSelector((s:RootState)=> s.auth);
+
+  const isAuth = access_token.length > 0? true: false
 
   const handleLogOut = ()=>{
     console.log("Logout");
@@ -14,15 +18,16 @@ export function Navbar() {
     navigate("/auth/login");
   }
 
+  
   return (
     <div className="flex justify-between items-center px-2 py-2 min-h-16 bg-slate-800 border-b-baby">
         <h1 className="italic capitalize text-4xl underline decoration-highlight">The best shop</h1>
         <nav>
             <ul>
-                <li>
+                <li> 
                   <button 
                   onClick={handleLogOut}
-                  className="button border-2 border-slate-100 font-bold flex gap-0.5"><LogOut/>Logout
+                  className={` ${isAuth ? "block":"invisible"} button border-2 border-slate-100 font-bold flex gap-0.5`}><LogOut/>Logout
                   </button>
                   </li>
             </ul>

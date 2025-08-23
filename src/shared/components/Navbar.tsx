1faad82b6460f6e0e34503c2c37acf5f5f010e19
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2025-08-23 11:21:49 -0600

    style: navbar buttons with transition styles.

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,8 +1,9 @@
-import { LogOut } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from '../../auth/store/authSlice';
 import { useNavigate } from 'react-router';
 import type { RootState } from '../../app';
+import { LogOut } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 
 export function Navbar() {
 
@@ -20,14 +21,19 @@ export function Navbar() {
 
   
   return (
-    <div className="flex justify-between items-center px-2 py-2 min-h-16 bg-slate-800 border-b-baby">
+    <div className="flex justify-between items-center px-2 py-2 min-h-16 bg-slate-300 dark:bg-slate-800 border-b-baby">
         <h1 className="italic capitalize text-4xl underline decoration-highlight">The best shop</h1>
         <nav>
-            <ul>
+            <ul className='flex gap-1.5'>
+                <li>
+                  <button className='button text-blue-900 dark:text-baby hover:text-highlight transition-all duration-200 ease-in '>
+                    <ShoppingCart/>
+                  </button>
+                </li>
                 <li> 
                   <button 
                   onClick={handleLogOut}
-                  className={` ${isAuth ? "block":"invisible"} button border-2 border-slate-100 font-bold flex gap-0.5`}><LogOut/>Logout
+                  className={` ${isAuth ? "block":"invisible"} button border-2 border-slate-100 font-bold flex gap-0.5 transition-all duration-200 ease-in hover:text-highlight hover:border-highlight`}><LogOut/>Logout
                   </button>
                   </li>
             </ul>
